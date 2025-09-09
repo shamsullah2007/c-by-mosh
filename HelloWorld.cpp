74c6c9847f46213bb6d77190564099605cb91d52
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-09 12:00:58 -0700

    working with strings

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,18 +1,22 @@
+#include <iomanip>
 #include <iostream>
 using namespace std;
 int main() {
 
 
-    int sales;
-    cout<<"enter the amount of sales:";
-    cin>>sales;
+    string street;
+    string city;
+    string state;
+    string country;
+    string country_code;
+    getline(cin, street);
+    getline(cin, city);
+    getline(cin, state);
+    getline(cin, country);
+    getline(cin, country_code);
+    cout<<setw(20)<<"STREET"<<setw(20)<<"CITY"<<setw(20)<<"STATE"<<setw(20)<<"COUNYRY"<<endl;
+    cout<<setw(20)<<street<<setw(20)<<city<<setw(20)<<state<<setw(20)<<country<<endl;
 
-    if (sales<=10000)
-        cout<<"comission"<<endl<<"$"<<sales*0.1;
-    else if (sales<=15000)
-        cout<<"comission"<<endl<<"$"<<sales*0.15;
-    else
-        cout<<"comission"<<endl<<"$"<<sales*0.2;
 
 
     return 0;
