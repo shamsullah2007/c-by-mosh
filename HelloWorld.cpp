03d643d4e719c7ca7107a1cd629ccb686d103b28
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-10 00:58:07 -0700

    calculator with if else

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,11 +2,29 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a ;
-    int b;
-    cin >> a >> b;
-    int z=(a>b)?a:b;
-    cout<<z<<endl;
+
+    int num1;
+    int num2;
+    string oper;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2>>oper;
+    if (oper=="+")
+        cout << num1 + num2;
+    else if (oper=="-")
+        cout << num1 - num2;
+    else if (oper=="*")
+        cout << num1 * num2;
+    else if (oper=="/")
+        if (num2!=0)
+           cout << num1 / num2;
+        else
+            cout<<"num2 cannot be 0";
+    else
+        cout<<"operation not vailaid";
+
+
+
+
 
 
     return 0;
