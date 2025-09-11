ab836c67132eb8c31248051eaa1fbde66f5d2775
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-11 00:45:19 -0700

    range based far loop

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,42 +1,24 @@
-#include <iomanip>
-#include <iostream>
+#include  <iostream>
 using namespace std;
 int main() {
+    // int numbers[]={1,2,3,4};
+    // for (int i=0;i<sizeof(numbers)/sizeof(int);i++) {
+    //     cout<<numbers[i]<<endl;
+    // }
+    // for (int number:numbers)
+    //     cout<<number<<endl;
 
-    int num1;
-    int num2;
-    char oper;
-    cout << "Enter two numbers: ";
-    cin >> num1 >> num2>>oper;
-    switch (oper) {
-        case '+':
-            cout << num1 + num2 << endl;
-            break;
-        case '-':
-            cout << num1 - num2 << endl;
-            break;
-        case '*':
-            cout << num1 * num2 << endl;
-            break;
-        case '/':
-            if (num2 != 0)
-                cout << num1 / num2 << endl;
-            else
-                cout <<"num2 cannot be 0";
-            break;
-        default:
-            cout<<"operation not vailid";
+    // double sum=0;
+    // double temprature[]={45,55.5,70,35.5};
+    // for (double temp:temprature)
+    //     sum+=temp;
+    // double avg=sum/(sizeof(temprature)/sizeof(double));
+    // cout<<avg<<endl;
 
+    string name="shams ullah";
+    for  (char nam:name)
+        cout << nam ;
 
+     return 0;
 
-
-
-    }
-
-
-
-
-
-
-    return 0;
 }
