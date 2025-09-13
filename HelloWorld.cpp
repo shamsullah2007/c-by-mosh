f98f1f5a9e449617a90d6033c25876f5f4df75b9
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-13 12:11:58 -0700

    function overloading

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,18 +2,29 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
+// Function overloading:
+// Function overloading is the phenomenon where functions have the same name
+// but different parameter lists (different number or types of parameters).
+//
+// Signature:
+// A function’s signature is defined by its name and its parameter list
+// (types, number, and order of parameters).
+// Functions can have the same name but must differ in their signature to be overloaded.
+//
+// Note: The return type is NOT part of the function signature in C++.
 
-double squre_root(int digits,double power=0.5) {
-    if (digits<0) {
-        cout<<pow(digits,power)<<"i";
-    }
-    else
-        cout<<pow(-digits,power);
+void greet(string name,int all) {
+    cout<<"hello"<<all<<name<<endl;
+}
+void greet(int all,string name) {
+    cout<<"hello"<<" "<<all<<"."<<name;
 }
 
 int main() {
-    unsigned int kamla=-10;
-    cout<<squre_root(kamla);
+    greet("shams",5);
+    greet(5,"mr");
+
+
 
 
 
