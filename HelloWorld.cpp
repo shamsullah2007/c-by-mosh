5dbe69646129f426249dd0191810b253c4d09937
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-14 22:46:55 -0700

    declaration of fuction

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
-const double taxrate=0.5;
-void calculateTax(double sale) {
-    cout<<taxrate*sale<<endl;
-}
+void calculateTax(double sale);//this is declaration of fuction that tells the compiler hey ther is function of the and the perameters
 int main() {
     double sale=10000;
-    
+
     calculateTax(sale);
     cout<<sale;
 
+}
+const double taxrate=0.5;
+// this is the definition of function the actual function
+void calculateTax(double sale) {
+    cout<<taxrate*sale<<endl;
 }
 
