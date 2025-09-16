da975f41b7b104cd943fef0efc066be450333353
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-16 09:07:43 -0700

    compying arrays

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
+#include <iterator> // for std::size
 using namespace std;
 
-#include  "utils/header.h"
-#include  "utils/header.h"
-using cct::calculateTax,cct::oddNumber;
-
 int main() {
-    double sale=10000;
-    calculateTax(sale);
-    cout<<sale;
-    oddNumber(sale);
+    int array[] = {0,1,2,3,4,5,6,7,8,9};
+    int second[size(array)];   // use std::size here
 
+    for (int i = 0; i < size(array); i++) {
+        second[i] = array[i];
+    }
+
+    for (int i = 0; i <size(array); i++) { // must use array, not second
+        cout << second[i] << endl;
+    }
+
+    return 0;
 }
-
-// this is the definition of function the actual function
-
-
