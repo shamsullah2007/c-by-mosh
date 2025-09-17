07753193aeeabd943c06c834252f1db7ce41417d
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-16 19:14:39 -0700

    searching arrays by linear searching algorithum

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <iterator> // for std::size
 using namespace std;
+int findIndex(int array[],int size,int target) {
+    for (int i = 0; i < size; i++)
+        if (array[i]==target)
+            return i;
+    return -1;
+}
 int main() {
-    int arrays[10]={1,2,3,4,5,6,7,8,9};
-    //unpacking : in c++ you need to know the exect size of arry you don't do partial unpacking
-    auto [x,y,z,d,g,h,j,e,r,t]=arrays;
-    cout<<"x:"<<x<<endl;
-    cout<<"y:"<<y<<endl;
-    cout<<"z:"<<z<<endl;
+    int arrays[]={1,2,3,4,5,6,5,8,9};
+
+    cout<<findIndex(arrays,size(arrays),9)<<endl;
+
+
+
 
 
     return 0;
