618cc7e096a90f987613f6df055307e981a72897
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-17 04:27:44 -0700

    how to sorting arrays

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 #include <iterator> // for std::size
 using namespace std;
-int findIndex(int array[],int size,int target) {
-    for (int i = 0; i < size; i++)
-        if (array[i]==target)
-            return i;
-    return -1;
+void swap(int arr[],int i,int j) {
+    int temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+
+
+}
+void sorting(int arr[],int size) {
+    for (int j=0;j<size;j++)
+        t {
+            if (arr[i] < arr[i - 1]) {
+                swap(arr, i, i - 1);
+
+            }
+    }
 }
 int main() {
-    int arrays[]={1,2,3,4,5,6,5,8,9};
-
-    cout<<findIndex(arrays,size(arrays),9)<<endl;
-
-
-
-
+    int array[]={2,1,6,5,9,8,0,10,99,87,65};
+    sorting(array,size(array));
+    for (int i = 0; i < size(array); i++) {
+        cout<<array[i]<<"\n";
+    }
 
     return 0;
 }
