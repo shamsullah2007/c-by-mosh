1c06f8de3c283d40a6d21cb5b200105937de1456
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-17 15:37:32 -0700

    passing pointers to a function

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <iterator> // for std::size
 using namespace std;
+// by passing function to a pointer  is old way
+void swap(int* first,int* second) {
+    int temp=*second;
+    *second=*first;
+    *first=temp;
+
+
+
+}
+// passing value to a function by refrence is modern way
+void swaping(int& price) {
+    price*=2;
+
+
+}
 int main(){
     //senerio where data is constant and pointer is not
-    const int x=100;
-    const int* p=&x;
-    int y=200;
-    p=&y;
-    cout<<*p<<endl;
-    // in this senerio pointer is constant
-    int y1=3444;
-    int* const p1=&y1;
-    *p1=200000;
-    int x1=3434;
-    cout<<*p1<<endl;
 
-    // senerio wher both are constant
-    const int d=7474;
-    const int* const p2=&d;
-    cout<<*p2<<endl;
+    int x=100;
+
+    // int y=200;;
+    // swap(&x,&y);
+    // cout<<x<<" "<<y<<endl;
+    swaping(x);
+    cout<<x<<endl;
+
+
 
 
 
