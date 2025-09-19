27511be64c6476b1271fdeda141869237f82ea43
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-18 18:08:25 -0700

    c strings

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
-#include <iterator> // for std::size
-#include <memory>
+#include <cstring>
 using namespace std;
-//arithemetics with pointers
-int main(){
-//  we have two types of pointer from previnting data leek
-/*shared pointer
- *a two differant pointer can loacate to a same memory location
- */
-    shared_ptr<int> p(new int(1));
-    shared_ptr<int> q(p);
-    if (p=q)
+int main() {
+    //WORKING WITH C STRINGS IN C THERE IS NO BUILT IN DATA TYPES FOR STRING YOU HAVE TO CREATE AN ARRAY OF CHAR
+    // with out \0 null terminating character it will be only array of char not the string
+    char name[6]={'s','a','m','s','h','\0'};
+    char name1[60]="shams";
+    char lastname[]="12345678";
+    cout<<name1<<endl;
+    cout<<strlen(name1)<<endl;
+    cout<<strcat(name1,lastname)<<endl;
+    strcpy(name1,lastname);
+    cout<<name1<<endl;
+    strcmp(name1,lastname);
+    if (strcmp(name1,lastname)==0) {
         cout<<"equal"<<endl;
+    }
 
-
-
-
-    return 0;
 
 }
