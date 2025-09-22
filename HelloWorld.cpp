f1681529101eaa66aa5ba6e68571dda88f2e541c
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-22 14:41:35 -0700

    arrays of struct

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <vector>
 using namespace std;
 using namespace this_thread;
 using namespace chrono;
+
 // structere is know as struct is custom data types made by the programer for complex calculation and so can be used in every functions
 struct CustUmer {
     int id=0;
@@ -12,16 +14,23 @@ struct CustUmer {
     };
 
 int main() {
-    CustUmer cust; // we make an instance of the custUmer so we can get the data type inside of it by .
-    cout<<"enter your id# ";
-    cin>>cust.id;
-    cout<<"enter your name# ";
-    cin>>cust.name;
-    cout<<"enter your address# ";
-    cin>>cust.address;
-    cout<<cust.id<<endl<<cust.name<<endl<<cust.address<<endl;
-    CustUmer cust2{5,"shams"};
-    cout<<cust2.id<<endl<<cust2.name<<endl<<cust2.address;
+    auto st3 = high_resolution_clock().now();
+    vector<CustUmer> custumers;
+    // CustUmer custumer={5,"shamsullah"};
+    // CustUmer custumer2={10,"shamsulla"};
+    custumers.push_back({5,"shamsullah"});
+    custumers.push_back({10,"shamsulla"});
+
+    for (const auto& ids: custumers) {
+        cout<<ids.name<<endl;
+
+    }
+    auto end=high_resolution_clock().now();
+    int duration = duration_cast<microseconds>(end-st3).count();
+    cout<<duration<<endl;
+
+
+
 
 
     return 0;
