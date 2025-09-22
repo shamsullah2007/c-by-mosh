0ee253f363b7b4be25950587a2f169bd6655f20f
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-22 05:32:58 -0700

    structure initialization

---
+++ utils/header.hpp
@@ -0,0 +1,6 @@
+// #ifndef shams_hft
+// #define shams_hft
+//this is declaration of fuction that tells the compiler hey ther is function of the and the perameters
+void calculateTax(double sale);
+// #endif
+
