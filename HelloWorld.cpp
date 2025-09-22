aa617b5a87b9dd4f5cfb0e32f025b1edbc98d535
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-22 05:28:27 -0700

    struct initialization

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,9 +6,9 @@ using namespace this_thread;
 using namespace chrono;
 // structere is know as struct is custom data types made by the programer for complex calculation and so can be used in every functions
 struct CustUmer {
-    int id;
+    int id=0;
     string name;
-    string address;
+    string address="shamsullajdjfdlkfja;";
     };
 
 int main() {
@@ -19,7 +19,10 @@ int main() {
     cin>>cust.name;
     cout<<"enter your address# ";
     cin>>cust.address;
-    cout<<cust.id<<endl<<cust.name<<endl<<cust.address;
+    cout<<cust.id<<endl<<cust.name<<endl<<cust.address<<endl;
+    CustUmer cust2{5,"shams"};
+    cout<<cust2.id<<endl<<cust2.name<<endl<<cust2.address;
+
 
     return 0;
 }
