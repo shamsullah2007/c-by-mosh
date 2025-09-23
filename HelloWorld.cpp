5666da332215e09f49c288b1c6ad81a9585c6c2b
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-22 23:11:48 -0700

    operators overloading in structure for two points

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -7,41 +7,35 @@ using namespace this_thread;
 using namespace chrono;
 
 // structere is know as struct is custom data types made by the programer for complex calculation and so can be used in every functions
-struct address {
-    string email;
-    string city="bannu";
-    string state="kpk";
-    string country="pakistan";
-    int postal_code;
+struct point {
+    int x;
+    int y;
 };
-struct CustUmer {
-    int id=0;
-    string name;
-    address address;
-    string job = "kota karahi";
-    };
-    bool operator==(const CustUmer & custumers, const CustUmer& custumers2) {
-        return (custumers.id==custumers2.id &&
-        custumers.name==custumers2.name &&
-        custumers.address.city==custumers2.address.city&&
-        custumers.job==custumers2.job);
+bool operator==(const point &a, const point &b) {
+    if (a.x==b.x && a.y==b.y)
+        return true;
+    else
+    {
+        return  false;
     }
-    ostream& operator<<(ostream& stream,CustUmer& custumer) {
-        stream<<custumer.name;
-        return stream;
-    }
+}
+ostream& operator<<(ostream& stream, const point& p) {
+    stream<<"("<<p.x<<","<<p.y<<")";
+    return stream;
+}
 
 
 int main() {
     auto st3 = high_resolution_clock().now();
-    CustUmer custumers{5,"shamsullah", {    "shamsullah@gmail.com",
-        "bannu","kpk","pakistan",45},"gaday ka kam kertay hat"};
-    CustUmer custumers2{5,"shamsullah", {    "shamsullah@gmail.com",
-        "bannu","kpk","pakistan",45,},"gaday ka kam kertay hat"};
-    if (custumers==custumers2) {
+    point p{45,45};
+    point p1{45,45};
+    if (p==p1){
         cout<<"equal"<<endl;
+
     }
-        cout<<custumers<<endl;
+    cout<<p<<endl;
+
+
 
         auto end=high_resolution_clock().now();
         int duration = duration_cast<microseconds>(end-st3).count();
