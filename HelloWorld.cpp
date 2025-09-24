41e03561d82ef22b1ee34d9563b43cf02e71add1
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-24 10:08:25 -0700

    enumeration is user define data type which are constant

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
 using namespace std;
-struct Movie {
-    string name;
-    string address;
-    int year;
-
+// enumeration is bassicaly the group of constant under one group and use for selection it is by default assigne value as integer
+enum  day {
+    sunday=1,
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday
 };
-ostream& operator<<(ostream& os, const Movie& movie) {
-    os<<movie.name<<" "<<movie.address<<" "<<movie.year;
-
-}
-Movie getMovie() {
-    Movie movie;
-    return movie;
-}
-void showMovie(Movie* movie) {
-    cout<<"Movie Name: "<<(*movie).name<<"  movie address  "<<movie->address<<endl;// you have to use pointer destructing because it is just an address so it don't have access to the mamber of the structure
-}
-
-
-
 int main() {
-    Movie movie{"shams","kotka muhammad shah",1947};
-    // cout<<movie<<endl;
-    showMovie(&movie);
+    int input;
+   const int sunday=9;
+    cin >> input;
 
 
 
+    switch(input) {
+        case day::sunday:
+        case day::saturday:
+            cout <<sunday<<" "<<saturday<<" are weekend days";
+            break;
+        case day::monday:
+            cout <<monday<<" "<<monday<<" are weekend days";
+            case day::tuesday:
+            cout<<tuesday;
+            case day::wednesday:
+            cout<<wednesday;
 
+            case day::friday:
+            cout<<friday;
+
+            cout<<"all are weed days";
+            break;
+        case sunday:
+            cout<<sunday<<" is local veriable";
+            break;
+    }
 
 }
