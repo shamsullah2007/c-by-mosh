a0ad427ffa7ebadfd66eee92f3636981b23436bf
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-25 10:46:54 -0700

    reading form csv file

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,35 +3,29 @@
 #include <iomanip>
 #include <vector>
 using namespace std;
+
 struct Movie {
     int id;
     string title;
     int year;
 };
 int main() {
-    vector<Movie> movies{
-        {1,"deadpole",2000},
-        {2,"deadpole o1",2004},
-        {3,"dEADPLE 02",2016}
-    };
-    movies.push_back({4, "Terminator 1", 1984});
-    movies.push_back({5, "Terminator 2", 1991});
-    ofstream file;
+    // READING FORM A FILE
+    ifstream file;
     file.open("hello.csv");
     if (file.is_open()) {
-        for (Movie movie:movies){
-            file <<movie.id<<","<< movie.title << "," << movie.year << endl;
+        while (!file.eof()) {
+            string line;
+            getline(file, line,',');
+            if (line.empty()){continue;}// if there is empty line or string we should skip it and move to the next iteration
+            Movie movie;
+            movie.id = stoi(line);
+            getline(file, line,',');
+            movie.title = line;
+            getline(file, line);
+            movie.year = stoi(line);
+            cout <<movie.title << endl;
+        }
 
-    }
-
-
-        file.close();
-        cout<<"data is written to csv file";
-
-
-
-    }
-    else {
-        cout<<"could not open file";
     }
 }
