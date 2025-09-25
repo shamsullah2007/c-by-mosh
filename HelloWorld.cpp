49d4bef7b1f42fb806a69423fede906609a4d256
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-25 00:23:27 -0700

    streams errors handling

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,13 +4,33 @@ using namespace std;
 int main() {
     // while reading data from a consol or file a temprary storage is make in the memory called buffer
     //[]
-    cout<<"first: ";
     int a;
-    cin >> a;
+    int  b;
+    while (true) {
+        cout<<"first: ";
+        cin >> a;
+        if (cin.fail()) {
+            cout<<"error"<<endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        else break;
 
-   cin.ignore(numeric_limits<streamsize>::max(),'\n');
-    cout<<"second: ";
-    int b;
-    cin >> b;
-    cout << a <<" "<<b << endl;
+
+    }  while (true) {
+        cout<<"second: ";
+        cin >> b;
+        if (cin.fail()) {
+            cout<<"error"<<endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        else break;
+
+
+
+    }
+    cout<<a<<b<<endl;
+    return 0;
+
 }
