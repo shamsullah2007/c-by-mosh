71d4fa424b12c861764804c2d0161f082fbb6448
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-25 09:43:51 -0700

    wrinting to a file

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
-#include <limits>
+#include <fstream>
+#include <iomanip>
+#include <vector>
 using namespace std;
-int Number(const string& prompt) {
-    int number;
-
-    while (true) {
-        cout<<prompt<<endl;
-        cin>>number;
-        if (cin.fail()) {
-            cout<<"invalid input"<<endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        }
-        else break;
-    }
-    return number;
-}
+struct Movie {
+    int id;
+    string title;
+    int year;
+};
 int main() {
-     int first=Number("first");
-    int second=Number("second");
+    vector<Movie> movies{
+        {1,"deadpole",2000},
+        {2,"deadpole o1",2004},
+        {3,"dEADPLE 02",2016}
+    };
+    movies.push_back({4, "Terminator 1", 1984});
+    movies.push_back({5, "Terminator 2", 1991});
+    ofstream file;
+    file.open("hello.csv");
+    if (file.is_open()) {
+        for (Movie movie:movies){
+            file <<movie.id<<","<< movie.title << "," << movie.year << endl;
 
-    // cout<<first<<second<<endl;
-    return 0;
+    }
 
+
+        file.close();
+        cout<<"data is written to csv file";
+
+
+
+    }
+    else {
+        cout<<"could not open file";
+    }
 }
