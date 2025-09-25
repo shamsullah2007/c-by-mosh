4a18d2c6efb46515ce65acd7f3307a9b53ed5ee4
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-24 19:41:00 -0700

    reding data to a stream

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,51 +1,16 @@
 #include <iostream>
+#include <limits>
 using namespace std;
-// strongly typed enumeratio is comes when you make another enumeration whith the same constant so it type reusablity error occure
-enum class   day {
-    sunday=1,
-    monday,
-    tuesday,
-    wednesday,
-    thursday,
-    friday,
-    saturday
-};enum  class month {
-    sunday=1,
-    monday,
-    tuesday,
-    wednesday,
-    thursday,
-    friday,
-    saturday
-};
-
 int main() {
-    int input;
-   const int sunday=9;
-    cin >> input;
+    // while reading data from a consol or file a temprary storage is make in the memory called buffer
+    //[]
+    cout<<"first: ";
+    int a;
+    cin >> a;
 
-
-
-    switch(input) {
-        case day::sunday:// what happens here is that the compiler don't  know how to compare int with sunday data type it don't know is it integer or some thing else
-        case day::saturday:
-            cout <<sunday<<" "<<saturday<<" are weekend days";
-            break;
-        case day::monday:
-            cout <<monday<<" "<<monday<<" are weekend days";
-            case day::tuesday:
-            cout<<tuesday;
-            case day::wednesday:
-            cout<<wednesday;
-
-            case day::friday:
-            cout<<friday;
-
-            cout<<"all are weed days";
-            break;
-        case sunday:
-            cout<<sunday<<" is local veriable";
-            break;
-    }
-
+   cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    cout<<"second: ";
+    int b;
+    cin >> b;
+    cout << a <<" "<<b << endl;
 }
