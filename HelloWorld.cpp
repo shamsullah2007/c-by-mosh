23771e2653b62aeddcce10116671438a7e127c65
shamsullah2007 <shasmullah2007a@gamil.com>
2025-09-26 14:45:59 -0700

    PARSING DATA OF A STRING

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,23 +5,45 @@
 #include <sstream>
 
 using namespace std;
-struct movie {
-    int id;
-    string name;
-    int year;
+struct Movie {
+  string title;
+  int year;
+
 };
-string convert(const double &num , int precision) {
-    stringstream ss;
-    ss<<fixed<<setprecision(precision)<<num<<endl;
-    return ss.str();
+Movie readMovie(string stream) {
+  stringstream ss;
+  ss.str(stream);
+  Movie movie;
+  getline(ss, movie.title, ',');
+  ss >> movie.year;
+  return movie;
+
 
 }
 
+
 int main() {
-    double num=50.5787;
-    int precision=2;
-    string name=convert(num,precision);
-    cout<<name<<endl;
+  string line="12 23 35";
+  stringstream ss;
+  ss.str(line);
+  int n;
+  ss>>n;
+  int m;
+  ss>>m;
+  int k;
+  ss>>k;
+  cout<<n<<" "<<m<<" "<<k<<endl;
+  string movie="terminator 1,1996";
+  stringstream Movie;
+  Movie.str(movie);
+  string a;
+  getline(Movie, a,',');
+  int b;
+  Movie>>b;
+  cout<<a<<endl<<b<<endl;
+  auto movies=readMovie(movie);
+  cout<<movies.title<<endl;
+  cout<<movies.year<<endl;
 
 
 
